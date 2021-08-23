0d129a71d8c9ef6d956c99d3dd6d534af844c5c3
BossOfThisGym <danila.solowyow2016@yandex.ru>
2021-08-23 16:25:07 +0300

    2.1

---
+++ home/box/web/hello.py
@@ -0,0 +1,12 @@
+from cgi import parse_qsl
+
+def HelloWorld(env,start_response):
+	d = parse_qsl(env['QUERY_STRING'])
+	output = ''
+	if env['REQUEST_METHOD'] == 'GET':
+		if env['QUERY_STRING'] != '':
+            for ch in d:
+            	output.append(ch)
+            	output.append('\n')
+	start_response('200 OK',[('Content-Type','text/plain')])
+	return output
